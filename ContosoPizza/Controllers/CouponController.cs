1b464de3bd2c400e3b23ef76d39fc19f4a8d9eb6
huurou <taihei.kitada@gmail.com>
2023-12-27 11:18:00 +0900

    Couponエンドポイント実装

---
+++ ContosoPizza/Controllers/CouponController.cs
@@ -0,0 +1,19 @@
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CouponController(PromotionsContext ctx) : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Coupon> Get()
+    {
+        return ctx.Coupons
+            .AsNoTracking()
+            .ToList();
+    }
+}
